b0bf051dab129190c651295bd5a08f480a841c53
wanggonghe <397856638@qq.com>
2022-05-07 12:26:57 +0800

    [M]:env

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { createVuePlugin } from 'vite-plugin-vue2'
+import { defineConfig, loadEnv } from 'vite'
 
-export default {
-  resolve: {
-    alias: {
-      '/@': '/src',
-      '/@public': '/public'
-    }
-  },
-  plugins: [createVuePlugin()]
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  console.log('env.VITE_BASEURL=', env.VITE_BASEURL)
+
+  return defineConfig({
+    baseUrl: env.VITE_BASEURL,
+    resolve: {
+      alias: {
+        '/@': '/src',
+        '/@public': '/public'
+      }
+    },
+    plugins: [createVuePlugin()]
+  })
+
+
 }
